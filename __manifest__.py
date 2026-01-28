ea0cccd6add31a09ac16a7c424eb2f94e40dff8d
Somyain <jsomya629@gmail.com>
2026-01-28 17:11:09 +0530

    security for user and hr role

--- __manifest__.py
+++ __manifest__.py
@@ -7,6 +7,7 @@
     """,
     'depends': ['base'],
     'data': [
+        'security/employee_field_security_groups.xml',
         'security/ir.model.access.csv',
         'views/employee_views.xml',
         'views/ir_sequence_data.xml',
--- models/my_model.py
+++ models/my_model.py
@@ -8,15 +8,29 @@ class EmployeeInfo(models.Model):
    reference_number = fields.Char(string='Sequence',copy=False,default=lambda self:_("New"),readonly=True, required=True)
    emp_name = fields.Char()
    age = fields.Integer(string="Age")
-   gender = fields.Selection([('male', 'Male'), ('female', 'Female'), ('other', 'Other')], string='Gender')   
+   gender = fields.Selection(
+       [('male', 'Male'), 
+        ('female', 'Female'), 
+        ('other', 'Other')
+        ], string='Gender')   
    phone = fields.Char(string="Phone", help="Enter phone number in international format")   
    partner_email = fields.Char(string="Email")
    dob = fields.Date(string="DOB")
    joining_date = fields.Datetime(string="Joining Date")
    description = fields.Text(string="Description")
    employee_image = fields.Binary(string="Employee Image")
-   job_title = fields.Char(string="job Title")
-   work_location = fields.Selection([('jaipur','Jaipur'),('ahemdabad','Ahemdabad')],string="Work Location")
+   employee_salary = fields.Integer( string='Employee Monthly income', required=True, tracking=True, help="Basic salary of the employee.")
+   job_title = fields.Selection(
+       [('software_dev','Software Developer'),
+        ('software_test','Software tester'),
+        ('hr','HR'),
+        ('ai/ml','AI/ML'),
+        ('odoo_professional','Odoo Professional')
+        ] , string="job Title")
+   work_location = fields.Selection(
+       [('jaipur','Jaipur'),
+        ('ahemdabad','Ahemdabad')
+        ],string="Work Location")
    emergency_con = fields.Char(string="Emergency contact", help="Enter phone number in international format")
    emergency_con_name = fields.Char(string="Emergency contact name")
    emergency_con_relation = fields.Char(string="Relation with emergency contact")
